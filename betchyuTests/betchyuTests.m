ea0257e6cc7acecf0090fc55d8ea80775be210a2
Adam Baratz <Betchyu@Adams-MacBook-Pro.local>
2013-12-05 12:11:16 -0500

    Initial Commit

---
+++ betchyuTests/betchyuTests.m
@@ -0,0 +1,34 @@
+//
+//  betchyuTests.m
+//  betchyuTests
+//
+//  Created by Adam Baratz on 12/5/13.
+//  Copyright (c) 2013 BetchyuLLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface betchyuTests : XCTestCase
+
+@end
+
+@implementation betchyuTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
