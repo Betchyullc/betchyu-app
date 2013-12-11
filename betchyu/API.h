5a736509558f2252c9e3130ad1d6e6f685422c17
Adam Baratz <Betchyu@Adams-MacBook-Pro.local>
2013-12-11 13:51:00 -0500

    ksfgf

---
+++ betchyu/API.h
@@ -0,0 +1,25 @@
+//
+//  API.h
+//  betchyu
+//
+//  Created by Adam Baratz on 12/11/13.
+//  Copyright (c) 2013 BetchyuLLC. All rights reserved.
+//
+
+#import "AFHTTPClient.h"
+#import "AFNetworking.h"
+
+//API call completion block with result as json
+typedef void (^JSONResponseBlock)(NSDictionary* json);
+
+@interface API : AFHTTPClient
+
+
++(API*)sharedInstance;
+
+//send an API command to the server
+-(void)post:(NSString *)path withParams:(NSMutableDictionary*)params onCompletion:(JSONResponseBlock)completionBlock;
+-(void)get:(NSString *)path withParams:(NSMutableDictionary*)params onCompletion:(JSONResponseBlock)completionBlock;
+
+
+@end
---
+++ betchyu/API.m
@@ -0,0 +1,73 @@
+//
+//  API.m
+//  betchyu
+//
+//  Created by Daniel Zapata on 12/11/13.
+//  Copyright (c) 2013 BetchyuLLC. All rights reserved.
+//
+
+#import "API.h"
+
+#define kAPIHost @"http://localhost:5000"
+#define kAPIPath @""
+
+@implementation API
+
+#pragma mark - Singleton methods
+/**
+ * Singleton methods
+ */
++(API*)sharedInstance
+{
+    static API *sharedInstance = nil;
+    static dispatch_once_t oncePredicate;
+    dispatch_once(&oncePredicate, ^{
+        sharedInstance = [[self alloc] initWithBaseURL:[NSURL URLWithString:kAPIHost]];
+    });
+    
+    return sharedInstance;
+}
+
+#pragma mark - init
+//intialize the API class with the destination host name
+
+-(API*)init
+{
+    //call super init
+    self = [super init];
+    
+    if (self != nil) {
+        
+        [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
+        
+        // Accept HTTP Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
+        [self setDefaultHeader:@"Accept" value:@"application/json"];
+    }
+    
+    return self;
+}
+
+
+-(void)commandWithParams:(NSMutableDictionary*)params onCompletion:(JSONResponseBlock)completionBlock
+{
+    NSMutableURLRequest *apiRequest =
+    [self multipartFormRequestWithMethod:@"POST"
+                                    path:kAPIPath
+                              parameters:params
+               constructingBodyWithBlock: ^(id <AFMultipartFormData>formData) {
+                   //TODO: attach file if needed
+               }];
+    
+    AFJSONRequestOperation* operation = [[AFJSONRequestOperation alloc] initWithRequest: apiRequest];
+    [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        //success!
+        completionBlock(responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        //failure :(
+        completionBlock([NSDictionary dictionaryWithObject:[error localizedDescription] forKey:@"error"]);
+    }];
+    
+    [operation start];
+}
+
+@end
