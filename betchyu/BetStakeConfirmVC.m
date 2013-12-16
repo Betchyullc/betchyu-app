2b31386a8b3d7b2e21557725ec010a22be49a49a
Adam Baratz <Betchyu@Adams-MacBook-Pro.local>
2013-12-16 13:54:58 -0500

    fb wall posts integration

--- betchyu/BetStakeConfirmVC.m
+++ betchyu/BetStakeConfirmVC.m
@@ -124,6 +124,7 @@
 
 - (void)friendPickerViewControllerSelectionDidChange:(FBFriendPickerViewController *)friendPicker {
     bet.friends = friendPicker.selection;
+    
 }
 
 // handles the user touching the done button on the FB friend selector
@@ -136,6 +137,57 @@
                           cancelButtonTitle:@"OK"
                           otherButtonTitles:nil] show];
     } else {
+        NSString* fid;
+        
+        // get facebook friend's ID from selection
+        for (id<FBGraphUser> user in bet.friends)
+        {
+            fid = user.id;
+            
+            //Make the post.
+            NSMutableDictionary* params = [[NSMutableDictionary alloc] initWithObjectsAndKeys:
+                                           @"Invitation to Betchyu", @"name",
+                                           @"I just made a goal on Betchyu! Do you think I can do it?", @"caption",
+                                           @"http://betchyu.com", @"link",
+                                           fid, @"to",
+                                           @"http://i.imgur.com/zq6D8lk.png", @"picture",
+                                           @"Betchyu", @"name", nil];
+            
+           // attemp to post the story to friends walls
+            [FBWebDialogs presentFeedDialogModallyWithSession:nil parameters:params handler:
+             ^(FBWebDialogResult result, NSURL *resultURL, NSError *error) {
+                if (error) {
+                    // Error launching the dialog or publishing a story.
+                    NSLog(@"Error publishing story.");
+                } else {
+                    if (result == FBWebDialogResultDialogNotCompleted) {
+                        // User clicked the "x" icon
+                        NSLog(@"User canceled story publishing.");
+                    } else {
+                        // Handle the publish feed callback
+                        NSDictionary *urlParams = [self parseURLParams:[resultURL query]];
+                        if (![urlParams valueForKey:@"post_id"]) {
+                            // User clicked the Cancel button
+                            NSLog(@"User canceled story publishing.");
+                        } else {
+                            // User clicked the Share button
+                            NSString *msg = [NSString stringWithFormat:
+                                             @"Posted story, id: %@",
+                                             [urlParams valueForKey:@"post_id"]];
+                            NSLog(@"%@", msg);
+                            // Show the result in an alert
+                            /*[[[UIAlertView alloc] initWithTitle:@"Result"
+                                                        message:msg
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK!"
+                                              otherButtonTitles:nil]
+                             show];*/
+                        }
+                    }
+                }
+            }];
+        }
+        
         BetFinalizeVC *vc = [[BetFinalizeVC alloc] initWithBet:bet];
         vc.title = @"Finalize Goal";
     
@@ -143,4 +195,19 @@
     }
 }
 
+
+/**
+ * A function for parsing URL parameters.
+ */
+- (NSDictionary*)parseURLParams:(NSString *)query {
+    NSArray *pairs = [query componentsSeparatedByString:@"&"];
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    for (NSString *pair in pairs) {
+        NSArray *kv = [pair componentsSeparatedByString:@"="];
+        NSString *val =
+        [kv[1] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+        params[kv[0]] = val;
+    }
+    return params;
+}
 @end
