0bfa307be4df38e8dd33e2383deaf91ebd16cfed
Adam Baratz <djz24@case.edu>
2014-06-25 15:39:16 -0400

    feedback page is finished

--- betchyu/Feedback.m
+++ betchyu/Feedback.m
@@ -16,22 +16,33 @@
     if (self) {
         // Initialization code
         self.owner = passedOwner;
+        int w = frame.size.width;
         
         int fontSize = 17;
-        if (frame.size.width > 500) {
+        if (w > 500) {
             fontSize = 22;
         }
         
         self.backgroundColor = [UIColor whiteColor];
         
         /// ui label containing the copy text
-        UILabel *copy = [[UILabel alloc] initWithFrame:CGRectMake(30, 35, frame.size.width-60, frame.size.height/2)];
+        UILabel *copy = [[UILabel alloc] initWithFrame:CGRectMake(30, 35, w-60, frame.size.height/3)];
         copy.font = FregfS;
         copy.text = @"Thanks for using Betchyu! We're very interested in what you have to say. Send us comments at info@betchyu.com.";
         copy.numberOfLines = 0;
         copy.textAlignment = NSTextAlignmentLeft;
         copy.textColor = Bdark;
-        [self addSubview:copy];        
+        [self addSubview:copy];
+        
+        UIView *line = [[UIView alloc] initWithFrame:CGRectMake(0, frame.size.height/3, w, 2)];
+        line.backgroundColor = Bmid;
+        [self addSubview:line];
+        
+        /// UIImageview for the logo
+        UIImageView *logo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"full-logo.png"]];
+        logo.frame = CGRectMake(w/4, copy.frame.size.height + 20, w/2, 45);
+        logo.alpha = 0.5f;
+        [self addSubview:logo];
     }
     return self;
 }
